c233fd4cbb279eef588701b3415ce465c0471fb4
Роман Княжик <romanknyazhyk@gmail.com>
2021-09-09 13:32:55 +0300

    Add project files.

---
+++ Graphics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Game_
+{
+    class Graphics
+    {
+        int topleft = 218;
+        const int hline = 196;
+        int topright = 191;
+        int vline = 179;
+        int bottomleft = 192;
+        int bottomright = 217;
+        public static void DrawChart(int x)
+        {
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding(28591);
+
+            for (int i = 1; i < x-1; i++)
+            {
+               Write(hline); 
+                
+            }
+        }
+
+         static void Write (int charcode)
+        {
+            Console.Write((char)charcode);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+
+
+namespace Roman_Game_
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int consWidth = 120;
+            int consHeight = 30;
+            char Line = ' ';
+           
+
+            Console.Title = "Game";
+            Console.SetWindowSize(consWidth, consHeight);
+            Console.SetBufferSize(consWidth, consHeight);
+            Console.CursorVisible = false;
+
+
+
+            char[] symb = new char[consWidth * consHeight];
+
+            for (int i = 0; i < consHeight * consWidth - 1; i++)
+            {
+                symb[i] = Line;
+            };
+
+            for (int i = 0; i < consHeight * consWidth; i++)
+            {
+                Console.Write(symb[i]);
+            }
+
+            Graphics.DrawChart(consWidth);
+            Console.ReadKey();
+
+        }
+
+        
+        
+
+
+            
+            
+
+
+    }
+}
+
